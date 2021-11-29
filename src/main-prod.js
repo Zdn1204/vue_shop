c7ebf792ba2865fc3b8ef0311ce8e3810235dcbd
Zdn1204 <2980425005@qq.com>
2021-11-29 13:02:33 +0800

    优化完成

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,45 +1,52 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// import './plugins/element.js'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
-//导入全局样式表
+// 导入全局样式表
 import './assets/css/global.css'
 
 import TreeTable from 'vue-table-with-tree-grid'
 // 导入副文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-// require styles导入副文本编辑器对应的样式
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+
+
+// 导入NProgress包对应的js和css
+import NProgress from 'nprogress'
 
 // 导入axios
 import axios from 'axios'
 // 配置请求的根路径
-axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
-axios.interceptors.request.use(config=>{
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 在request拦截器中，展示进度条NProgress.start()
+axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  NProgress.start()
+  return config
+})
+// 在response拦截器中，隐藏进度条NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 
 Vue.config.productionTip = false
-Vue.component('tree-table',TreeTable)
+Vue.component('tree-table', TreeTable)
 // 将副文本编辑器，注册为全局可用的组件
 Vue.use(VueQuillEditor)
 Vue.prototype.$http = axios
 
-Vue.filter('dateFormat',function(originVal){
+Vue.filter('dateFormat', function (originVal) {
   const dt = new Date(originVal)
 
   const y = dt.getFullYear()
-  const m = (dt.getMonth() + 1 + '').padStart(2,'0')
-  const d = (dt.getDate() + '').padStart(2,'0')
-  
-  const hh = (dt.getHours() + '').padStart(2,'0')
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
   const mm = (dt.getMinutes() + '').padStart(2, '0')
-  const ss = (dt.getSeconds() + '').padStart(2,'0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
 
   return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
 })
